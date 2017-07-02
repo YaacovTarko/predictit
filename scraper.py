a8ce08cade58b9e87ac1a406f6282cdbb898222f
Yaacov Tarko <yaacov.tarko@gmail.com>
2017-07-02 10:33:40 -0700

    Fixed exception handling on timeout.

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ db = client.predictit_data
 hist = db.historical
 
 url = "https://www.predictit.org/api/marketdata/all/"
+prev_entries = {}
 
 
 while(True):
@@ -16,7 +17,9 @@ while(True):
 	try:
 		ret = requests.get(url, timeout = 10).text
 	except Exception:
-		sleep(1800)
+		print("Timeout")
+		time.sleep(1800)
+		continue
 	for market in json.loads(ret)["Markets"]:
 		for contract in market["Contracts"]:
 			entry = { "name" : contract["LongName"],
@@ -28,7 +31,9 @@ while(True):
 					"LastClosePrice" : contract["LastClosePrice"],
 					"Time" : str(datetime.datetime.now())
 			}
-			post_id = hist.insert_one(entry)
+			if (entry["name"] in prev_entries and entry != prev_entries[entry["name"]]):
+				post_id = hist.insert_one(entry)
+			prev_entries[entry["name"]]= entry
 
 	finish = datetime.datetime.now()
 	print("Data Collected at %s" % str(finish) )
